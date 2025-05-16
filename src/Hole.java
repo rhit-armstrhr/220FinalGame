0325c1bcab0f64b6616dfac8c278d6a5337a5374
rhit-lamantfj <lamantfj@rose-hulman.edu>
2025-05-16 13:58:29 -0400

    Scoreboard edits

--- src/Hole.java
+++ src/Hole.java
@@ -3,6 +3,7 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JComponent;
@@ -19,6 +20,8 @@ public class Hole extends JPanel {
 	private int par;
 	private BufferedImage image;
 	private double startX, startY;
+	private ArrayList<BufferedImage> bigNums;
+	private BufferedImage HoleCard;
 
 	public Hole(String file, int holeNum, int par, double startX, double startY) {
 		this.holeNum = holeNum;
@@ -26,8 +29,10 @@ public class Hole extends JPanel {
 		this.startX = startX;
 		this.startY = startY;
 		System.out.println("Hole 1");
+		bigNums = Animations.bigNums();
 		try {
 			image = ImageIO.read(new File(file));
+			HoleCard = ImageIO.read(new File("src/Images/HoleCounter.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("Failed to Load Image for hole" + holeNum);
@@ -42,6 +47,8 @@ public class Hole extends JPanel {
 	// return what to draw and have a central drawing area
 	public void draw(Graphics2D g2) {
 		g2.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), this);
+		g2.drawImage(HoleCard, 10, 10, HoleCard.getWidth(), HoleCard.getHeight(), this);
+		g2.drawImage(bigNums.get(holeNum), 20, 40, bigNums.get(holeNum).getWidth(), bigNums.get(holeNum).getHeight(), this);
 		repaint();
 	}
 
