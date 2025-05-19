c350aac7f592401c95cabf6d091e7b2a0d313968
rhit-lamantfj <lamantfj@rose-hulman.edu>
2025-05-19 16:34:18 -0400

    Finished Ball animation and reworked movement

--- src/Ball.java
+++ src/Ball.java
@@ -16,12 +16,14 @@ public class Ball extends JPanel {
 	private ArrayList<BufferedImage> ballFrames = Animations.ballArc();
 	private int currentFrame = 0;
 	private boolean moving;
-	
-    private int frameCounter = 0;
-    private final int FRAME_DELAY = 5;  
-    private final double SPEED = 8;     
-    private double destX, destY;
-    private double speedx, speedy;
+
+	private int frameCounter = 0;
+	private final int FRAME_DELAY = 3;
+	private final double SPEED = 8;
+	private double destX, destY;
+	private double speedx, speedy;
+	private double totalDist, halfDist, traveledDist;
+	private int countDirection = 1;
 
 	public Ball(double startX, double startY) {
 		x = (int) startX;
@@ -29,60 +31,86 @@ public class Ball extends JPanel {
 		height = 0;
 
 	}
-	
+
 	public Boolean checkPin(double l, double r, double b, double t) {
-		//l = left, r = right, b = bottom, t = top of the border of the pin hole location for each hole passed in. 
-		if(x <=r && l<=x && y<=b&&t<=y) {
+		// l = left, r = right, b = bottom, t = top of the border of the pin hole
+		// location for each hole passed in.
+		if (x <= r && l <= x && y <= b && t <= y) {
 			return true;
 		}
 		return false;
 	}
 
-	public void getBlown(double x,double y) {
-		
+	public void getBlown(double x, double y) {
+
 	}
 
 	public void getHit(double xLoc, double yLoc) {
-		destX =  xLoc;
+		destX = xLoc;
 		destY = yLoc;
 		double dx = destX - x;
-        double dy = destY - y;
-        double dist = Math.hypot(dx, dy);
-        speedx = SPEED * dx / dist;
-        speedy = SPEED * dy / dist;
-        moving = true;
-	
+		double dy = destY - y;
+		totalDist = Math.hypot(dx, dy);
+		halfDist = totalDist / 2;
+		speedx = SPEED * dx / totalDist;
+		speedy = SPEED * dy / totalDist;
+		moving = true;
+
 	}
-	
+
 	public void draw(Graphics2D g2) {
-		g2.drawImage(ballFrames.get(currentFrame), (int)x,(int) y,ballFrames.get(currentFrame).getWidth(), ballFrames.get(currentFrame).getHeight(), this);
+		g2.drawImage(ballFrames.get(currentFrame), (int) x, (int) y, ballFrames.get(currentFrame).getWidth(),
+				ballFrames.get(currentFrame).getHeight(), this);
 		repaint();
 	}
 
 	public void nextHole(double startX, double startY) {
-		x =  startX;
-		y =  startY;
+		x = startX;
+		y = startY;
+	}
+	
+	public boolean checkIfMoving() {
+		return moving;
 	}
 
 	public void update() {
-		if(!moving) return;
-		
+		if (!moving)
+			return;
+
+		double step = Math.hypot(speedx, speedy);
+		x += speedx;
+		y += speedy;
+		traveledDist += step;
+
 		double xdist = destX - x;
 		double ydist = destY - y;
-		if(Math.hypot(xdist, ydist) <= SPEED) {
+		if (Math.hypot(xdist, ydist) <= SPEED) {
 			x = destX;
 			y = destY;
 			moving = false;
-		} else {
-			x += speedx;
-			y += speedy;
-			
+			traveledDist = 0;
+			currentFrame = 0;
+			System.out.println("stopped");
+		}
+
+		if (Math.abs(traveledDist) >= Math.abs(halfDist)) {
+			countDirection = -1;
+		}else {
+			countDirection = 1;
 		}
 		
-		frameCounter++;
-		if(frameCounter >= FRAME_DELAY) {
-			frameCounter = 0;
-            currentFrame = (currentFrame + 1) % ballFrames.size();
+		if (currentFrame <= 0) {
+			currentFrame = 0;
+			countDirection = 1;
 		}
+		
+		frameCounter ++;
+		if (frameCounter >= FRAME_DELAY) {
+			frameCounter = 0;
+			currentFrame = (currentFrame + countDirection) % ballFrames.size();
+			System.out.println(currentFrame + " and count" + countDirection);
+		}
+//		System.out.println(frameCounter);
+		
 	}
 }
